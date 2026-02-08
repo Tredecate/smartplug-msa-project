05e1307283208e5f8540ff33e69402a2daec800f
Thomas <thomasdezwart@gmail.com>
2026-02-08 02:18:49 -0800

    Initial version of processing service

---
+++ app.py
@@ -0,0 +1,193 @@
+import httpx
+import json
+import logging.config
+import connexion
+
+from pathlib import Path
+from datetime import datetime, timezone, timedelta
+from apscheduler.schedulers.background import BackgroundScheduler
+from config_handler import APP_CONFIG, API_CONFIG, STORAGE_CONFIG, LOG_CONFIG
+
+
+def get_stats():
+    logger.info("GET /stats request received")
+
+    # Read stats and remove unwanted fields
+    stats_dict = read_dict_from_file(APP_CONFIG["stats_file"])
+    stats_dict.pop("last_updated", None)
+
+    # Error if stats_dict is somehow empty
+    if not stats_dict:
+        logger.error("Statistics file is nonexistent, empty, or malformed")
+        return ({"message": "Statistics do not exist"}, 404)
+    
+    logger.debug(f"Returning stats: {stats_dict}")
+    logger.info("GET /stats request completed")
+
+    # Return stats_dict
+    return (stats_dict, 200)
+
+
+def init_scheduler():
+    scheduler = BackgroundScheduler(daemon=True)
+    
+    scheduler.add_job(
+        func=           update_statistics, 
+        trigger=        'interval', 
+        seconds=        APP_CONFIG["proc_interval_secs"], 
+        next_run_time=  datetime.now() + timedelta(seconds=2)
+    )
+
+    scheduler.start()
+
+
+def update_statistics():
+    logger.info("Periodic statistics update triggered")
+
+    # Read current stats and determine time range for new data
+    current_stats = read_dict_from_file(APP_CONFIG["stats_file"])
+    current_time = datetime.now(timezone.utc)
+    latest_time = datetime.fromisoformat(current_stats.get("last_updated", APP_CONFIG["fallback_start_time"]))
+
+    # Go fish
+    energy_data, temp_data = fetch_data(latest_time + timedelta(microseconds=1), current_time)
+
+    logger.info(f"Fetched {len(energy_data)} new energy records and {len(temp_data)} new temperature records")
+
+    # Update stats and write to disk
+    new_stats = calculate_stats(current_stats, energy_data, temp_data)
+    write_dict_to_file(APP_CONFIG["stats_file"], new_stats)
+
+    logger.info("Periodic statistics update completed")
+
+
+def fetch_data(start_timestamp: datetime, end_timestamp: datetime) -> tuple[list[dict], list[dict]]:
+    # Define query parameters for the time range
+    timespan_params = {"start_timestamp": start_timestamp.isoformat(timespec='microseconds'), "end_timestamp": end_timestamp.isoformat()}
+
+    try:
+        # Gone fishing
+        energy_res =    httpx.get(STORAGE_CONFIG["baseurl"] + STORAGE_CONFIG["endpoints"]["energy"],    params=timespan_params)
+        temp_res =      httpx.get(STORAGE_CONFIG["baseurl"] + STORAGE_CONFIG["endpoints"]["temp"],      params=timespan_params)
+
+        # Check for HTTP errors and log them
+        if energy_res.status_code != 200:
+            raise Exception(f"Error fetching energy data: {energy_res.status_code} {energy_res.text}")
+        if temp_res.status_code != 200:
+            raise Exception(f"Error fetching temperature data: {temp_res.status_code} {temp_res.text}")
+        
+    except Exception as e:
+        # If anything broke, error and return empty
+        logger.error(f"Data fetch failed! {e}")
+        return ([], [])
+    
+    # Return the fish--uhhhhh data
+    return (energy_res.json(), temp_res.json())
+
+
+def calculate_stats(old_stats: dict, energy_data: list[dict], temp_data: list[dict]) -> dict:
+    # Define a base dict with defaults
+    base = {
+        "num_energy_readings": 0,
+        "min_energy_consumed": float('inf'),
+        "max_energy_consumed": float('-inf'),
+        "avg_energy_consumed": 0,
+
+        "num_temp_readings": 0,
+        "min_internal_temp": float('inf'),
+        "max_internal_temp": float('-inf'),
+        "avg_internal_temp": 0,
+
+        "last_updated": APP_CONFIG["fallback_start_time"]
+    }
+
+    # Overlay old_stats on top of base to replace defaults with actual values (if they exist)
+    base.update(old_stats)
+    
+    # Calculate cumulative reading counts
+    total_energy_readings = len(energy_data) + base["num_energy_readings"]
+    total_temp_readings = len(temp_data) + base["num_temp_readings"]
+    
+    # Extract datapoint lists from new readings
+    new_energy_readings = [reading["energy_consumed_watt_minutes"] for reading in energy_data]
+    new_temp_readings = [reading["internal_temp_celsius"] for reading in temp_data]
+    new_timestamps = [datetime.fromisoformat(reading["date_created"]) for reading in energy_data + temp_data]
+
+    # If we've got new energy readings...
+    if new_energy_readings:
+        energy_stats = {
+            # Calculate cumulative stats using old stats and new readings
+            "num_energy_readings": total_energy_readings,
+            "min_energy_consumed": min([*new_energy_readings, base["min_energy_consumed"]]),
+            "max_energy_consumed": max([*new_energy_readings, base["max_energy_consumed"]]),
+
+            # Calculate new average using weighted average of old and new readings
+            "avg_energy_consumed": (sum(new_energy_readings) + (base["avg_energy_consumed"] * base["num_energy_readings"])) / total_energy_readings
+        }
+    
+    # If we've got new temp readings...
+    if new_temp_readings:
+        temp_stats = {
+            # Calculate cumulative stats using old stats and new readings
+            "num_temp_readings": total_temp_readings,
+            "min_internal_temp": min([*new_temp_readings, base["min_internal_temp"]]),
+            "max_internal_temp": max([*new_temp_readings, base["max_internal_temp"]]),
+
+            # Calculate new average using weighted average of old and new readings
+            "avg_internal_temp": (sum(new_temp_readings) + (base["avg_internal_temp"] * base["num_temp_readings"])) / total_temp_readings
+        }
+
+    new_stats = {
+        # Write any stats we have to a new dict, prioritizing new stats
+        **old_stats,
+        **(energy_stats if new_energy_readings  else {}),
+        **(temp_stats   if new_temp_readings    else {}),
+
+        # Set last_updated to the latest timestamp from the new readings, or keep old if no new readings
+        "last_updated": max([*new_timestamps, datetime.fromisoformat(base["last_updated"])]).isoformat()
+    }
+    
+    logger.debug(f"Updated stats: {new_stats}" if new_stats != old_stats else "No stats changed")
+    return new_stats
+
+
+def read_dict_from_file(file_name: str) -> dict:
+    # If the file doesn't exist, return an empty dict
+    if not Path(file_name).is_file():
+        return {}
+    
+    try:
+        # If the file DOES exist: read, parse, and return
+        with open(Path(file_name), 'r') as json_file:
+            return json.load(json_file)
+    except:
+        # If the file is malformed or inaccessible, return an empty dict
+        return {}
+
+
+def write_dict_to_file(file_name: str, content: dict):
+    # Make sure parent directories exist
+    Path(file_name).parent.mkdir(parents=True, exist_ok=True)
+    
+    # Open file_name in write mode
+    with open(Path(file_name), "w") as json_file:
+        # Parse dict to JSON and dump it to disk
+        json.dump(content, json_file)
+
+
+# SETUP LOGGING
+Path(LOG_CONFIG["handlers"]["file"]["filename"]).parent.mkdir(parents=True, exist_ok=True)
+logging.config.dictConfig(LOG_CONFIG)
+logger = logging.getLogger("basicLogger")
+
+
+# SETUP CONNEXION APP
+app = connexion.FlaskApp(__name__, specification_dir=API_CONFIG["spec_dir"])
+app.add_api(API_CONFIG["file"], 
+            strict_validation=API_CONFIG["strict_validation"], 
+            validate_responses=API_CONFIG["validate_responses"])
+
+
+if __name__ == "__main__":
+    init_scheduler()
+    app.run(port=APP_CONFIG["port"])
---
+++ config_handler.py
@@ -0,0 +1,110 @@
+import yaml
+from pathlib import Path
+
+# DEFAULT CONFIGURATIONS
+_DEFAULT_CONFIG = {
+    "version": 1,
+    "api": {
+        "file": "openapi.yml",
+        "spec_dir": "./",
+        "strict_validation": True,
+        "validate_responses": True
+    },
+    "services": {
+        "self": {
+            "port": 8100,
+            "proc_interval_secs": 60,
+            "stats_file": "./data/stats.json",
+            "fallback_start_time": "1970-01-01T00:00:00Z"
+        },
+        "storage": {
+            "baseurl": "http://localhost:8090",
+            "endpoints": {
+                "energy": "/plug-data/energy-consumed",
+                "temp": "/plug-data/internal-temp"
+            }
+        }
+    }
+}
+
+_DEFAULT_LOG_CONFIG = {
+    "version": 1,
+    "formatters": {
+        "simple": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "simple",
+            "stream": "ext://sys.stdout"
+        },
+        "file": {
+            "class": "logging.FileHandler",
+            "level": "DEBUG",
+            "formatter": "simple",
+            "filename": "./logs/app.log"
+        }
+    },
+    "loggers": {
+        "basicLogger": {
+            "level": "DEBUG",
+            "handlers": ["console", "file"],
+            "propagate": False
+        },
+        "root": {
+            "level": "DEBUG",
+            "handlers": ["console"]
+        }
+    },
+    "disable_existing_loggers": False
+}
+
+
+# UTIL FUNCTIONS
+def overlay_dicts(base: dict, overlay: dict) -> dict:
+    """
+    Recursively overlays one dictionary onto another
+    """
+    # Go through key:value pairs in the overlay dict
+    for key, value in overlay.items():
+        # If value in both dict is a dict, recurse
+        if key in base and isinstance(base[key], dict) and isinstance(value, dict):
+            base[key] = overlay_dicts(base[key], value)
+        else:
+            # Otherwise just set the value
+            base[key] = value
+    return base
+
+
+# LOAD CONFIGURATION
+# App config
+try:
+    with open(Path("./config/app_conf.yml"), 'r') as f:
+        file_config = yaml.safe_load(f)
+except FileNotFoundError:
+    file_config = {}
+
+# Log config
+try:
+    with open(Path("./config/log_conf.yml"), 'r') as f:
+        file_log_config = yaml.safe_load(f)
+except FileNotFoundError:
+    file_log_config = {}
+
+
+# OVERLAY LOADED CONFIG OVER DEFAULTS
+config_dict = overlay_dicts(_DEFAULT_CONFIG, file_config)
+log_config_dict = overlay_dicts(_DEFAULT_LOG_CONFIG, file_log_config)
+
+
+# SET GLOBAL CONFIG VARIABLES
+# App config
+APP_CONFIG = config_dict["services"]["self"]
+STORAGE_CONFIG = config_dict["services"]["storage"]
+API_CONFIG = config_dict["api"]
+
+# Log config
+LOG_CONFIG = log_config_dict
