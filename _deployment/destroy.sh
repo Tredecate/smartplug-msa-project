1074fa7966ef336ca86e60fb5cbbd9db94efb13d
Tredecate <thomasdezwart@gmail.com>
2026-04-01 16:35:58 -0700

    Deploy: Format and add comments to teardown script

--- _deployment/destroy.sh
+++ _deployment/destroy.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
 set -e
+
+# Run terraform destroy
 cd terraform
 terraform destroy -auto-approve -var-file="../.key_files/ssh_key_path.tfvars"
+
+# Remove generated files
 rm -rf ../.key_files
 rm -f ../ansible/inventory.ini
+
+# Finish
 echo "Deployment destroyed successfully."
